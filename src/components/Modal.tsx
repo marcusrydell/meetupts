cbacf17112fa3f7ae63526985ff5665f5b7703b8
Robin Lans <roblan95@icloud.com>
2022-01-24 16:46:54 +0100

    change the data

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -20,22 +20,16 @@ function Modals({ event }: Props): JSX.Element {
             open={true}
             className="p-5"
         >
-            <h1 className="text-5xl">Comments</h1>
+            <h1>Comments</h1>
             {comments.map((comment: string, index: number) => {
                 return <Segment key={"comment" + index}>{comment}</Segment>;
             })}
-            <div className="flex w-full justify-center">
+            <div>
                 <input
                     onChange={(event) => setInputText(event.target.value)}
-                    className="w-2/3 h-10"
                     placeholder="Write a comment"
                 />
-                <button
-                    onClick={() => {}}
-                    className="w-auto bg-blue-300 p-3 rounded"
-                >
-                    Add comment
-                </button>
+                <button onClick={() => {}}>Add comment</button>
             </div>
         </Modal>
     );
