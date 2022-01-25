4d4ad65388cfb03eabadf713533e9d649f1d07a6
Rockyfelle <rockyfelle@live.se>
2022-01-25 21:32:58 +0100

    Add a variety of tests

--- src/tests/Comment.test.tsx
+++ src/tests/Comment.test.tsx
@@ -1,4 +1,32 @@
 import { render, screen } from "@testing-library/react";
+import App from "../App";
+import userEvent from '@testing-library/user-event'
 
 // User Story
 // Som användare vill jag kunna kommentera på varje möte
+describe("Event component", () => {
+	it("opens comment modal", () => {
+		render(<App />);
+
+		userEvent.click(screen.getByText('Bilkrockar'))
+
+		const headerEl = screen.getByText("Comments");
+
+        expect(headerEl).toBeInTheDocument();
+	});
+
+	it("posts and renders a comment", () => {
+		render(<App />);
+
+		userEvent.click(screen.getByText('Bilkrockar'))
+
+		userEvent.type(screen.getByPlaceholderText('Write a comment'), 'Test x32');
+
+		userEvent.click(screen.getByText('Add comment'));
+
+		const comment = screen.getByText('Test x32');
+
+		expect(comment).toBeInTheDocument();
+	});
+
+});
--- src/tests/Event.test.tsx
+++ src/tests/Event.test.tsx
@@ -2,6 +2,8 @@ import { render, screen, cleanup } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import Event from "../components/Event";
 import { IEvent } from "../model/Events";
+import userEvent from '@testing-library/user-event';
+import App from "../App";
 
 // User Story
 // SomSom användare vill jag kunna anmäla mig till ett möte.
@@ -38,4 +40,27 @@ describe("Event component", () => {
         expect(locationEl).toBeInTheDocument();
         expect(timeEl).toBeInTheDocument();
     });
+
+	it("joins an event and changes the button to say leave", () => {
+        render(<App />);
+
+		userEvent.click(screen.getAllByText('Join')[0]);
+
+		const leaveButton = screen.getAllByText('Leave')[1];
+
+		expect(leaveButton).toBeInTheDocument();
+	});
+
+	it("state persists between reloads", () => {
+        render(<App />);
+
+		userEvent.click(screen.getAllByText('Join')[0]);
+
+		render(<App />);
+
+		const leaveButton2 = screen.getAllByText('Leave')[4];
+
+		expect(leaveButton2).toBeInTheDocument();
+	});
+
 });
