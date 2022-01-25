4704ad6b33d6afc0b7177639d16f303c771b5304
Robin Lans <roblan95@icloud.com>
2022-01-25 13:49:46 +0100

    test

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,33 @@ import Modal from "./components/Modal";
 import { context } from "./context/context";
 
 function App() {
-    const { events, setEvents } = useContext(context);
+    // const { events, setEvents } = useContext(context);
+    const [events, setEvents] = useState<IEvent[]>([
+        {
+            id: 1,
+            name: "Bilkrockar",
+            joined: true,
+            location: "Angered",
+            time: "2022-01-14",
+            comments: ["Felle Krockar i Angered", "Köpa nya bil"],
+        },
+        {
+            id: 2,
+            name: "Dreamhack",
+            joined: false,
+            location: "Orten",
+            time: "2021-12-14",
+            comments: ["JOlt Cola och svett", "Dataspel"],
+        },
+        {
+            id: 3,
+            name: "Snusträff",
+            joined: false,
+            location: "Luleå",
+            time: "2021-05-14",
+            comments: ["Vilken rolig träff", "Gott med snus"],
+        },
+    ]);
     const [modal, setModal] = useState<IEvent[]>([]);
     const [showModal, setShowModal] = useState<boolean>(false);
 
